3e59e25c0d462bcb138de99278422be5e183065c
Sarah Davison <sarah.davison619@gmail.com>
2023-06-05 15:44:31 -0500

    added abbreviateTwoWordName.js

---
+++ abbreviateTwoWordName.js
@@ -0,0 +1,1 @@
+const abbrevName = name => name.split(' ').map( x => x.toUpperCase().slice(0, 1)).join('.')
--- fineTheUniqueNumber.js
+++ fineTheUniqueNumber.js
@@ -1,7 +1,15 @@
-function findUniq(arr) {
-    let sortedArr = arr.sort( (a, b) => a - b)
-    if (sortedArr[0] === sortedArr[1]){
-      return sortedArr[sortedArr.length - 1] 
-    }
-    return sortedArr[0]
-  }
+<div id="donate-button-container">
+<div id="donate-button"></div>
+<script src="https://www.paypalobjects.com/donate/sdk/donate-sdk.js" charset="UTF-8"></script>
+<script>
+PayPal.Donation.Button({
+env:'production',
+hosted_button_id:'AUKTEQEA7S4JG',
+image: {
+src:'https://www.paypalobjects.com/en_US/i/btn/btn_donateCC_LG.gif',
+alt:'Donate with PayPal button',
+title:'PayPal - The safer, easier way to pay online!',
+}
+}).render('#donate-button');
+</script>
+</div>
