da2c6b3e7f18abc87ed139e4d3d57c153f2b61f4
Sarah Davison <sarah.davison619@gmail.com>
2023-08-01 21:24:18 -0500

    added determineChromosomalSex.js

---
+++ determineChromosomalSex.js
@@ -0,0 +1,6 @@
+function chromosomeCheck(sperm) {
+  if (sperm.includes('Y')){
+    return "Congratulations! You're going to have a son."
+  }
+  return "Congratulations! You're going to have a daughter."
+}
